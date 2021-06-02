90a4b46d8d7caa9f988c85c8004ff35956af208a
Your Name <you@example.com>
2021-06-02 15:10:16 +0300

    commit

--- js/js-13-test/task-2/calculator.test.js
+++ js/js-13-test/task-2/calculator.test.js
@@ -4,7 +4,7 @@ it('should get squared numbers', () => {
     const result = getSquaredNumbers([1, 2, 3]);
 
     expect(result).toEqual([1, 4, 9]);
-})
+});
 
 it('should keep odd numbers only', () => {
   const result = getOddNumbers([1, 5, 3, 8, 10]);
