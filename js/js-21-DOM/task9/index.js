91611827fe87d50355c105bec713ab4282504761
Your Name <you@example.com>
2021-06-11 23:41:48 +0300

    commit

---
+++ js/js-21-DOM/task9/index.js
@@ -0,0 +1,22 @@
+export const finishList = () => {
+  const listBlock = document.querySelector('.list');
+  const listElem5 = document.querySelector('.special');
+
+  const listElem1 = document.createElement('li');
+  listElem1.textContent = '1';
+  listBlock.prepend(listElem1);
+
+  const listElem4 = document.createElement('li');
+  listElem4.textContent = '4';
+  listElem4.before(listElem5);
+
+  const listElem6 = document.createElement('li');
+  listElem6.textContent = '6';
+  listElem5.after(listElem6);
+
+  const listElem8 = document.createElement('li');
+  listElem8.textContent = '8';
+  listBlock.append(listElem8);
+};
+
+finishList();
