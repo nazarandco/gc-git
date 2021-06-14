86fa29bfa2b7f74748cee57a982b4452144a2374
Your Name <you@example.com>
2021-06-14 12:35:12 +0300

    commit

---
+++ js/js-21-DOM/task11/index.js
@@ -0,0 +1,15 @@
+const manageClasses = () => {
+  const one = document.querySelector('.one');
+  one.classList.add('selected');
+
+  const two = document.querySelector('.two');
+  two.classList.remove('selected');
+
+  const three = document.querySelector('.three');
+  three.classList.toggle('three_done');
+
+  const four = document.querySelector('.some-class');
+  four.classList.add('another-class');
+};
+
+manageClasses();
