9cdef314c53b14c6eb15d83b78948db30517043b
Your Name <you@example.com>
2021-06-15 20:34:03 +0300

    commit

---
+++ js/js-23-events/task2/index.js
@@ -0,0 +1,73 @@
+const generateNumbersRange = (from, to) => {
+  const result = [];
+
+  for (let i = from; i <= to; i += 1) {
+    result.push(i);
+  }
+
+  return result;
+};
+
+const getLineSeats = () =>
+  generateNumbersRange(1, 10)
+    .map(
+      (seatNumber) => `
+    <div 
+    class="sector__seat" 
+    data-seat-number="${seatNumber}"
+    ></div>
+    `
+    )
+    .join('');
+
+const getSectorLines = () => {
+  const seatsStr = getLineSeats();
+  return generateNumbersRange(1, 10)
+    .map(
+      (lineNumber) => `
+    <div 
+    class="sector__line" 
+    data-line-number="${lineNumber}"
+    >${seatsStr}</div>
+    `
+    )
+    .join('');
+};
+
+const arenaEl = document.querySelector('.arena');
+
+const renderArena = () => {
+  const linesStr = getSectorLines();
+
+  const sectorStr = generateNumbersRange(1, 3)
+    .map(
+      (sectorNumber) => `
+    <div 
+    class="sector" 
+    data-sector-number="${sectorNumber}"
+    >${linesStr}</div>
+    `
+    )
+    .join('');
+
+  arenaEl.innerHTML = sectorStr;
+};
+
+const onSeatSelect = (event) => {
+  const isSeat = event.target.classList.contains('sector__seat'); // зверни увагу, тут без крапки ! це не звертання, а пошук строки
+
+  if (!isSeat) {
+    return;
+  }
+
+  const seatNumber = event.target.dataset.seatNumber;
+  const lineNumber = event.target.closest('.sector__line').dataset.lineNumber;
+  const sectorNumber = event.target.closest('.sector').dataset.sectorNumber;
+
+  const selectedSeatElem = document.querySelector('.board__selected-seat');
+  selectedSeatElem.textContent = `S ${sectorNumber} - L ${lineNumber} - S ${seatNumber}`;
+};
+
+arenaEl.addEventListener('click', onSeatSelect);
+
+renderArena();
