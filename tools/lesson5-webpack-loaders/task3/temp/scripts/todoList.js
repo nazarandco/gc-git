3c3f32c5e4b7038504f4704bd4d5c86d32e1bbbb
Your Name <you@example.com>
2021-07-04 20:43:17 +0300

    commit

---
+++ tools/lesson5-webpack-loaders/task3/temp/scripts/todoList.js
@@ -0,0 +1,8 @@
+import { onCreateTask } from './createTask.js';
+import { onListClick } from './updateTask.js';
+export const initTodoListHandlers = () => {
+  const createBtnElem = document.querySelector('.create-task-btn');
+  createBtnElem.addEventListener('click', onCreateTask);
+  const todoListElem = document.querySelector('.list');
+  todoListElem.addEventListener('click', onListClick);
+};
