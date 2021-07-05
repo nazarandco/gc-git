92ff683f5f400839d03d9754300b5d922bcd2186
Your Name <you@example.com>
2021-07-05 18:08:42 +0300

    commit

--- react/lesson1/task3/src/index.js
+++ react/lesson1/task3/src/index.js
@@ -6,9 +6,9 @@ import './styles.css';
 const rootElement = document.querySelector('#root');
 
 const element = (
-  <div className="greeting">
-    <div className="greeting__title">Hello, world!</div>
-    <div className="greeting__text">I'm learning React</div>
+  <div className='greeting'>
+    <div className='greeting__title'>Hello, world!</div>
+    <div className='greeting__text'>I'm learning React</div>
   </div>
 );
 
