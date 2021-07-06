37be1df0b906199dd3b59eb1624ab7c3d7e52bb7
Your Name <you@example.com>
2021-07-06 20:18:09 +0300

    commit

--- react/lesson3/task4/src/App.jsx
+++ react/lesson3/task4/src/App.jsx
@@ -1,15 +1,17 @@
 import React from 'react';
 import Greeting from './Greeting.jsx';
 
-const UserInfo = {
-  firstName: 'John',
-  lastName: 'Doe',
-  birthDate: new Date('2004-01-01T11:11:11.819Z')
-};
+// const UserInfo = {
+//   firstName: 'John',
+//   lastName: 'Doe',
+//   birthDate: new Date('2004-01-01T11:11:11.819Z'),
+// };
 
 const App = () => (
   <Greeting
-    userData={UserInfo}
+    firstName='John'
+    lastName='Doe'
+    birthDate={{new Date('2004-01-01T11:11:11.819Z')}}
   />
 );
 
--- react/lesson3/task4/src/Greeting.jsx
+++ react/lesson3/task4/src/Greeting.jsx
@@ -2,10 +2,10 @@ import React from 'react';
 
 const Greeting = (props) => {
   const userYears = Math.floor(
-    (new Date() - props.userData.birthDate) / 1000 / 60 / 60 / 24 / 365
+    (new Date() - props.birthDate) / 1000 / 60 / 60 / 24 / 365
   );
   return (
-    <div className='greeting'>{`My name is ${props.userData.firstName} ${props.userData.lastName}. I'm ${userYears} years old`}</div>
+    <div className='greeting'>{`My name is ${props.firstName} ${props.lastName}. I'm ${userYears} years old`}</div>
   );
 };
 
