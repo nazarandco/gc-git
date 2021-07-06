d8832121bf9731de426be2bd1875fb8d2d916c53
Your Name <you@example.com>
2021-07-06 17:24:07 +0300

    commit

--- react/lesson3/task1/src/index.jsx
+++ react/lesson3/task1/src/index.jsx
@@ -1,20 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Search from './Search.jsx';
 import './index.scss';
 import './search.scss';
 
 const rootElement = document.querySelector('#root');
 
-const SearchField = (props) => (
-  <>
-    <div className='search'>
-      <h1 className='search__title'>{`Hello, ${props.name}. What to search for you?`}</h1>
-      <div className='search__field'>
-        <input type='text' className='search__input' />
-        <button className='search__button'>Search</button>
-      </div>
-    </div>
-  </>
-);
-
-ReactDOM.render(<SearchField name="Nazik"/>, rootElement);
+ReactDOM.render(<Search name="Nazik"/>, rootElement);
