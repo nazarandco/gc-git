37563749cdfdc55f38d20cdc6fdfd68d0bc812ec
Your Name <you@example.com>
2021-07-07 15:39:57 +0300

    commit

--- react/lesson5/task2/src/GoodButton.jsx
+++ react/lesson5/task2/src/GoodButton.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 
 class GoodButton extends React.Component {
-  handleClick() {
-    alert('Click me!');
+  handleClick(e) {
+    alert(e.target.textContent);
   }
 
   render() {
