b5701fa1064b26ddd8fe98f7c777ee412a603574
Your Name <you@example.com>
2021-07-07 15:37:01 +0300

    commit

---
+++ react/lesson5/task2/src/index.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import GoodButton from './GoodButton.jsx';
+import './index.scss';
+
+const rootElement = document.querySelector('#root');
+
+ReactDOM.render(<GoodButton />, rootElement);
