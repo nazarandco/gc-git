54451c08b9fb6200a361f839490b88a9a1654d9c
Your Name <you@example.com>
2021-07-09 17:15:10 +0300

    commit

--- react/lesson6/task2/src/Mailbox.jsx
+++ react/lesson6/task2/src/Mailbox.jsx
@@ -1,14 +1,12 @@
 import React from 'react';
 
-const Mailbox = ({ unreadMessage }) => {
-  return (
-    <div className='mailbox'>
-      <span className='mailbox__text'>Messages</span>
-      {unreadMessage.length > 0 && 
+const Mailbox = ({ unreadMessage }) => (
+  <div className='mailbox'>
+    <span className='mailbox__text'>Messages</span>
+    {unreadMessage.length > 0 && (
       <span className='mailbox__count'>{unreadMessage.length}</span>
-      }
-    </div>
-  );
-}
+    )}
+  </div>
+);
 
 export default Mailbox;
