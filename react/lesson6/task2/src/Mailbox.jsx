9318d70d9360a725d9195a085b5197734bdfd9d6
Your Name <you@example.com>
2021-07-09 17:16:22 +0300

    commit

--- react/lesson6/task2/src/Mailbox.jsx
+++ react/lesson6/task2/src/Mailbox.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
 
-const Mailbox = ({ unreadMessage }) => (
+const Mailbox = ({ unreadMessages }) => (
   <div className='mailbox'>
     <span className='mailbox__text'>Messages</span>
-    {unreadMessage.length > 0 && (
-      <span className='mailbox__count'>{unreadMessage.length}</span>
+    {unreadMessages.length > 0 && (
+      <span className='mailbox__count'>{unreadMessages.length}</span>
     )}
   </div>
 );
--- react/lesson6/task2/src/index.jsx
+++ react/lesson6/task2/src/index.jsx
@@ -5,4 +5,4 @@ import Mailbox from './Mailbox.jsx';
 
 const rootElement = document.querySelector('#root');
 
-ReactDOM.render(<Mailbox unreadMessage={[2, 1]}/>, rootElement);
+ReactDOM.render(<Mailbox unreadMessages={[2, 1]}/>, rootElement);
