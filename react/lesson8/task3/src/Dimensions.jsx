bc978ea8e67292827606f334ed3a29c74a9ff0d2
Your Name <you@example.com>
2021-07-14 22:06:31 +0300

    commit

--- react/lesson8/task3/src/Dimensions.jsx
+++ react/lesson8/task3/src/Dimensions.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-class Dimension extends React.Component {
+class Dimensions extends React.Component {
   state = {
     width: null,
     height: null,
@@ -9,34 +9,29 @@ class Dimension extends React.Component {
   componentDidMount() {
     window.addEventListener('resize', this.onResize);
     const { innerWidth, innerHeight } = window;
-    document.title = `${innerWidth} x ${innerHeight}`;
     this.setDimensions(innerWidth, innerHeight);
   }
-
-  componentWillUnmount() {
-    window.removeEventListener('resize', this.onResize);
-  }
-
-  setDimensions = (width, height) => {
-    this.setState({
-      width,
-      height,
-    });
-    document.title = `${width} x ${height}`;
-  };
 
   onResize = (e) => {
     const { innerWidth, innerHeight } = e.target;
     this.setDimensions(innerWidth, innerHeight);
   };
 
+  setDimensions = (width, height) => {
+    this.setState({
+      width,
+      height,
+    });
+    document.title = `${innerWidth} x ${innerHeight}`;
+  };
+
+  componentWillUnmount() {
+    window.removeEventListener('resize', this.onResize);
+  }
+
   render() {
-    return (
-      <div>
-        {`${this.state.width} x ${this.state.height}`}
-      </div>
-    );
+    return <div>{`${this.state.width}px - ${this.state.height}px`}</div>;
   }
 }
 
-export default Dimension;
+export default Dimensions;
--- react/lesson8/task3/src/index.jsx
+++ react/lesson8/task3/src/index.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Dimension from './Dimensions.jsx';
+import Dimensions from './Dimensions.jsx';
 import './index.scss';
 
 const rootElement = document.querySelector('#root');
 
-ReactDOM.render(<Dimension />, rootElement);
+ReactDOM.render(<Dimensions />, rootElement);
