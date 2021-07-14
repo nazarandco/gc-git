0bf6128bf9fa6033f13ca6d5a91c998749377a6c
Your Name <you@example.com>
2021-07-14 16:13:35 +0300

    commit

---
+++ react/lesson8/task1/src/Life.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import moment from 'moment';
+
+class Life extends React.Component {
+  constructor(props) {
+    super(props);
+    console.log('constructor: good place to create state');
+  }
+
+  componentDidMount() {
+    console.log('componentDidMount: API calls, subscriptions');
+  }
+
+  shouldComponentUpdate(nextProps, nextState) {
+    console.log(nextProps, nextState);
+    console.log('shouldComponentUpdate(nextProps, nextState): decide to render or not to render');
+    return nextProps.number % 2;
+  }
+
+  componentDidUpdate(previousProps, previousState) {
+    console.log(previousProps);
+    console.log('componentDidUpdate(previousProps, previousState): some updates based on new props');
+  }
+
+  componentWillUnmount() {
+    console.log('componentWillUnmount(): cleanup before DOM related to component will be removed');
+  }
+
+  render() {
+    console.log('return React element to build DOM');
+    return <div className='number'>{this.props.number}</div>;
+  }
+}
+
+export default Life;
