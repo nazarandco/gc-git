bd0fb3b8669ef05792474527383bedc4f9b7d51c
Your Name <you@example.com>
2021-07-15 11:27:20 +0300

    commit

--- react/lesson8/task5/src/ConnectionStatus.jsx
+++ react/lesson8/task5/src/ConnectionStatus.jsx
@@ -29,9 +29,9 @@ class ConnectionStatus extends React.Component {
 
   render() {
     return this.state.status === 'online' ? (
-      <div class='status status_online'>{this.state.status}</div>
+      <div className='status'>{this.state.status}</div>
     ) : (
-      <div class='status status_offline'>{this.state.status}</div>
+      <div className='status status_offline'>{this.state.status}</div>
     );
   }
 }
