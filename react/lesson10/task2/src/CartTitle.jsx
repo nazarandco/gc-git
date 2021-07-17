976e8ec36b5220bb4e36d0e5400f78e3f650c6f6
Your Name <you@example.com>
2021-07-17 14:57:49 +0300

    commit

--- react/lesson10/task2/src/CartTitle.jsx
+++ react/lesson10/task2/src/CartTitle.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const CartTitle = ({ userData, count }) => (
-  <div class='cart-title'>{`${userData.firstName}, you added ${count} items`}</div>
+  <div class='cart-title'>{`${userData.firstName} ${userData.lastName}, you added ${count} items`}</div>
 );
 
 export default CartTitle;
