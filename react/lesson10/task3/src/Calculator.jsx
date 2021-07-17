08ca15733ae82f27c76c9b5f6ffb6c264d17d1e7
Your Name <you@example.com>
2021-07-17 17:16:46 +0300

    commit

---
+++ react/lesson10/task3/src/Calculator.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class Calculator extends React.Component {
+  render() {
+    return (
+      <div>
+        <TemperatureInput scale='c' />
+        <TemperatureInput scale='f' />
+      </div>
+    );
+  }
+}
+
+export default Calculator;
