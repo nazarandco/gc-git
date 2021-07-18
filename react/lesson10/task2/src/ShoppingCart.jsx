16fe3632f3e79f19ace9ff62dceb64c819cf1518
Your Name <you@example.com>
2021-07-18 16:48:42 +0300

    commit

--- react/lesson10/task2/src/ShoppingCart.jsx
+++ react/lesson10/task2/src/ShoppingCart.jsx
@@ -3,20 +3,24 @@ import ProductsList from './ProductsList.jsx';
 import CartTitle from './CartTitle.jsx';
 
 class ShoppingCart extends React.Component {
-  state = {
-    cartItems: [
-      {
-        id: '1',
-        name: 'iPhone 11',
-        price: 999,
-      },
-      {
-        id: '2',
-        name: 'iPad Pro',
-        price: 799,
-      },
-    ],
-  };
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      cartItems: [
+        {
+          id: '1',
+          name: 'iPhone 11',
+          price: 999,
+        },
+        {
+          id: '2',
+          name: 'iPad Pro',
+          price: 799,
+        },
+      ],
+    };
+  }
 
   render() {
     const count = this.state.cartItems.length;
