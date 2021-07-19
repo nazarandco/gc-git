236ea0a12f47ea7f81b1c807fa85ea7d6ba36f5b
Your Name <you@example.com>
2021-07-19 17:17:43 +0300

    commit

--- react/lesson11/task3/src/Expand.jsx
+++ react/lesson11/task3/src/Expand.jsx
@@ -1,7 +1,4 @@
 import React from 'react';
-// import { faChevronUp } from '@fortawesome/free-solid-svg-icons';
-// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-//     <FontAwesomeIcon icon={faChevronUp} />
 
 const Expand = ({ children, title, condition, showExpand, hideExpand }) => (
   <div className='expand border'>
@@ -9,18 +6,16 @@ const Expand = ({ children, title, condition, showExpand, hideExpand }) => (
       <span className='expand__title'>{title}</span>
       <button
         className={
-          condition === true
+          condition
             ? 'expand__toggle-btn expand__toggle-btn-off'
             : 'expand__toggle-btn'
         }
-        onClick={condition === true ? hideExpand : showExpand}
+        onClick={condition ? hideExpand : showExpand}
       >
         <i className='fas fa-chevron-down'></i>
       </button>
     </div>
-    <div className='expand__content'>
-      {condition === true ? children : null}
-    </div>
+    <div className='expand__content'>{condition ? children : null}</div>
   </div>
 );
 
