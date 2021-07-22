f5ee954a5f2eb5227b2de704a9731b1087ef9063
Your Name <you@example.com>
2021-07-22 15:24:27 +0300

    commit

---
+++ react/lesson14/task2/src/App.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import { BrowserRouter, Route, Link, Switch } from 'react-router-dom';
+import User from './User.jsx';
+
+const App = () => {
+  return (
+    <div className='page'>
+      <div className='page__content'>
+        <BrowserRouter>
+          <h1>Users</h1>
+          <ul className='navigation'>
+            <li className='navigation__item'>
+              <Link to='/users/github'>Github</Link>
+            </li>
+            <li className='navigation__item'>
+              <Link to='/users/facebook'>Facebook</Link>
+            </li>
+          </ul>
+
+          <Switch>
+            <Route path='/users/:userId'>
+              <User />
+            </Route>
+            <Route path='/'>
+              <span>Select a user please</span>
+            </Route>
+          </Switch>
+        </BrowserRouter>
+      </div>
+    </div>
+  );
+};
+
+export default App;
