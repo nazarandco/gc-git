2fcee120bc3b8c886256df8276ea4e36c5b26ef8
Your Name <you@example.com>
2021-08-06 14:42:27 +0300

    commit

--- redux/2-react-intro/src/App.jsx
+++ redux/2-react-intro/src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import Header from './Header.jsx';
-// import { UserDataContext } from './userData';
+import { UserDataContext } from './userData';
 
 class App extends React.Component {
   state = {
