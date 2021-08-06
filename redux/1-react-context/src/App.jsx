2e2a31a18b4418284739eb799d0431a178cfe1ae
Your Name <you@example.com>
2021-08-06 13:36:38 +0300

    commit

--- redux/1-react-context/src/App.jsx
+++ redux/1-react-context/src/App.jsx
@@ -4,7 +4,7 @@ import { themes, ThemeContext } from './themes-context';
 
 class App extends React.Component {
   state = {
-    theme: themes.dark,
+    theme: themes.light,
   };
 
   toggleTheme = () => {
