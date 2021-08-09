103ecea668c7f919d5a40c9c7ff3a0cffb13db9a
Your Name <you@example.com>
2021-08-09 14:08:58 +0300

    commit

--- redux/2-react-intro/task1/src/index.js
+++ redux/2-react-intro/task1/src/index.js
@@ -22,10 +22,3 @@ const counterReducer = (state = 0, action) => {
 };
 
 const store = createStore(counterReducer);
-
-store.dispatch(increment());
-store.dispatch(increment());
-store.dispatch(increment());
-store.dispatch(decrement());
-
-console.log(store.getState());
