d569cca3f840aaab8a53ee8b08404bea6edcb6c4
Your Name <you@example.com>
2021-08-09 14:07:38 +0300

    commit

---
+++ redux/2-react-intro/task1/src/index.js
@@ -0,0 +1,31 @@
+import { createStore } from 'redux';
+
+const increment = () => {
+  return {
+    type: 'COUNTER/INCREMENT',
+  };
+};
+const decrement = () => {
+  return {
+    type: 'COUNTER/DECREMENT',
+  };
+};
+const counterReducer = (state = 0, action) => {
+  switch (action.type) {
+    case 'COUNTER/INCREMENT':
+      return state + 1;
+    case 'COUNTER/DECREMENT':
+      return state - 1;
+    default:
+      return state;
+  }
+};
+
+const store = createStore(counterReducer);
+
+store.dispatch(increment());
+store.dispatch(increment());
+store.dispatch(increment());
+store.dispatch(decrement());
+
+console.log(store.getState());
