7f2aa6179db7c96f1d0b596f75b778e8f4b9d1d1
Your Name <you@example.com>
2021-08-10 14:04:32 +0300

    commit

--- redux/2-react-intro/task3/src/users.reducer.js
+++ redux/2-react-intro/task3/src/users.reducer.js
@@ -7,12 +7,10 @@ const usersReducer = (state = initialState, action) => {
   switch (action.type) {
     case ADD:
       return {
-        ...state,
         usersList: state.usersList.concat(action.payload),
       };
     case DELETE:
       return {
-        ...state,
         usersList: state.usersList.filter((user) => user.id !== action.payload),
       };
     default:
