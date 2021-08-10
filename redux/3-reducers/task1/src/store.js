b9f6ff5c914ababfd843e30f12d322d74c07196c
Your Name <you@example.com>
2021-08-10 17:49:20 +0300

    commit

--- redux/3-reducers/task1/src/store.js
+++ redux/3-reducers/task1/src/store.js
@@ -7,6 +7,9 @@ const appReducer = combineReducers({
   users: usersReducer,
 });
 
-const store = createStore(appReducer);
+const store = createStore(
+  appReducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
 
 export default store;
---
+++ redux/3-reducers/task2/src/counter.js
@@ -0,0 +1,32 @@
+import { createStore } from 'redux';
+
+const INCREMENT = 'COUNTER/INCREMENT';
+const DECREMENT = 'COUNTER/DECREMENT';
+
+const increment = () => {
+  return {
+    type: INCREMENT,
+  };
+};
+
+const decrement = () => {
+  return {
+    type: DECREMENT,
+  };
+};
+
+const counterReducer = (state = 0, action) => {
+  switch (action.type) {
+    case INCREMENT:
+      return state + 1;
+    case DECREMENT:
+      return state - 1;
+    default:
+      return state;
+  }
+};
+
+export const store = createStore(
+  counter, 
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
